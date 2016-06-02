c1dbbbcbd30e6b17787107ebca5742c94af6abb0
Eugene Obrezkov <ghaiklor@gmail.com>
2016-06-02 15:45:39 +0300

    docs: add comments to boot and multiboot

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,5 +1,10 @@
+;; This code will be called by the bootloader
+;; In our case it will be Grub2
+
+;; start will be our entry point to the kernel
 global start
 
+;; by default, text section is the default section for executable code
 section .text
 bits 32
 
