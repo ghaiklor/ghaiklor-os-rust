b699e47ad09f3450b8db2eeb4df31ee79de691ba
Eugene Obrezkov <ghaiklor@gmail.com>
2016-06-03 10:42:01 +0300

    bootstrap: add script for building objconv and grub

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,34 +32,79 @@ osx() {
     fi
   fi
   echo "Running setup script..."
+  brew install gmp mpfr libmpc autoconf automake
   brew install nasm
   brew tap djphoenix/gcc_cross_compilers
-  brew install djphoenix/gcc_cross_compilers/x86_64-elf-binutils
+  brew install djphoenix/gcc_cross_compilers/x86_64-elf-binutils djphoenix/gcc_cross_compilers/x86_64-elf-gcc
+
+  # Start building objconv and grub
+  export PREFIX="$HOME/opt/"
+  export TARGET=x86_64-elf
+  export PATH="$PREFIX/bin:$PATH"
+
+  mkdir -p $HOME/src
+  mkdir -p $PREFIX
+  mkdir -p $PREFIX/bin
+
+  # objconv
+  cd $HOME/src
+
+  if [ ! -d "objconv" ]; then
+    curl http://www.agner.org/optimize/objconv.zip > objconv.zip
+    mkdir -p build-objconv
+    unzip objconv.zip -d build-objconv
+
+    cd build-objconv
+    unzip source.zip -d src
+    g++ -o objconv -O2 src/*.cpp --prefix="$PREFIX"
+    cp objconv $PREFIX/bin/objconv
+  fi
+
+  # grub
+  cd $HOME/src
+
+  if [ ! -d "grub" ]; then
+    git clone --depth 1 git://git.savannah.gnu.org/grub.git
+
+    cd grub
+    sh autogen.sh
+    mkdir -p build-grub
+    cd build-grub
+    ../configure --disable-werror TARGET_CC=$TARGET-gcc TARGET_OBJCOPY=$TARGET-objcopy \
+    TARGET_STRIP=$TARGET-strip TARGET_NM=$TARGET-nm TARGET_RANLIB=$TARGET-ranlib --target=$TARGET --prefix=$PREFIX
+    make
+    make install
+  fi
 }
 
 archLinux() {
   echo "Detected Arch Linux"
   echo "You can help me and write bootstrap script for your OS"
+  exit
 }
 
 ubuntu() {
   echo "Detected Ubuntu/Debian"
   echo "You can help me and write bootstrap script for your OS"
+  exit
 }
 
 fedora() {
   echo "Detected Fedora"
   echo "You can help me and write bootstrap script for your OS"
+  exit
 }
 
 suse() {
   echo "Detected a suse"
   echo "You can help me and write bootstrap script for your OS"
+  exit
 }
 
 gentoo() {
   echo "Detected Gentoo Linux"
   echo "You can help me and write bootstrap script for your OS"
+  exit
 }
 
 endMessage() {
