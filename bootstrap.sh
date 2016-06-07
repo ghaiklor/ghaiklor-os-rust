67461b58fabf4cba51cd3a699a09247ca2fa3f7f
Eugene Obrezkov <ghaiklor@gmail.com>
2016-06-07 10:25:37 +0300

    bootstrap: refactor bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,77 +10,64 @@ banner() {
 osx() {
   echo "Detected OSX!"
   if [ ! -z "$(which brew)" ]; then
-    echo "Homebrew detected! Now updating..."
+    # Install all dependencies through brew
     brew update
-    if [ -z "$(which git)" ]; then
-      echo "Now installing git..."
-      brew install git
+    brew install qemu
+    brew install gmp mpfr libmpc autoconf automake
+    brew install nasm
+    brew install xorriso
+    brew tap djphoenix/gcc_cross_compilers
+    brew install djphoenix/gcc_cross_compilers/x86_64-elf-binutils djphoenix/gcc_cross_compilers/x86_64-elf-gcc
+
+    # Start building objconv and grub
+    # There are no formulas for brew, so compiling from sources
+    export PREFIX="$HOME/opt/"
+    export TARGET=x86_64-elf
+    export PATH="$PREFIX/bin:$PATH"
+
+    mkdir -p $HOME/src
+    mkdir -p $PREFIX
+    mkdir -p $PREFIX/bin
+
+    # objconv
+    cd $HOME/src
+
+    if [ ! -d "objconv" ]; then
+      curl http://www.agner.org/optimize/objconv.zip > objconv.zip
+      mkdir -p build-objconv
+      unzip objconv.zip -d build-objconv
+
+      cd build-objconv
+      unzip source.zip -d src
+      g++ -o objconv -O2 src/*.cpp --prefix="$PREFIX"
+      cp objconv $PREFIX/bin/objconv
     fi
-    if [ -z "$(which qemu-system-x86_64)" ]; then
-      echo "Installing qemu..."
-      brew install qemu
+
+    # grub
+    cd $HOME/src
+
+    if [ ! -d "grub" ]; then
+      git clone --depth 1 git://git.savannah.gnu.org/grub.git
+
+      cd grub
+      sh autogen.sh
+      mkdir -p build-grub
+      cd build-grub
+      ../configure --disable-werror TARGET_CC=$TARGET-gcc TARGET_OBJCOPY=$TARGET-objcopy TARGET_STRIP=$TARGET-strip TARGET_NM=$TARGET-nm TARGET_RANLIB=$TARGET-ranlib --target=$TARGET --prefix=$PREFIX
+      make
+      make install
     fi
+
+    # Install rust
+    export PATH="$HOME/.cargo/bin"
+
+    curl https://sh.rustup.rs -sSf | sh
+    rustup override add nightly
   else
-    echo "Homebrew does not appear to be installed! Would you like me to install it?"
-    printf "(Y/n): "
-    read -r installit
-    if [ "$installit" == "Y" ]; then
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    else
-      echo "Will not install, now exiting..."
-      exit
-    fi
+    echo "Homebrew does not appear to be installed!"
+    echo "Install Homebrew and run this script again"
+    exit
   fi
-  echo "Running setup script..."
-  brew install gmp mpfr libmpc autoconf automake
-  brew install nasm
-  brew install xorriso
-  brew tap djphoenix/gcc_cross_compilers
-  brew install djphoenix/gcc_cross_compilers/x86_64-elf-binutils djphoenix/gcc_cross_compilers/x86_64-elf-gcc
-
-  # Start building objconv and grub
-  export PREFIX="$HOME/opt/"
-  export TARGET=x86_64-elf
-  export PATH="$PREFIX/bin:$PATH"
-
-  mkdir -p $HOME/src
-  mkdir -p $PREFIX
-  mkdir -p $PREFIX/bin
-
-  # objconv
-  cd $HOME/src
-
-  if [ ! -d "objconv" ]; then
-    curl http://www.agner.org/optimize/objconv.zip > objconv.zip
-    mkdir -p build-objconv
-    unzip objconv.zip -d build-objconv
-
-    cd build-objconv
-    unzip source.zip -d src
-    g++ -o objconv -O2 src/*.cpp --prefix="$PREFIX"
-    cp objconv $PREFIX/bin/objconv
-  fi
-
-  # grub
-  cd $HOME/src
-
-  if [ ! -d "grub" ]; then
-    git clone --depth 1 git://git.savannah.gnu.org/grub.git
-
-    cd grub
-    sh autogen.sh
-    mkdir -p build-grub
-    cd build-grub
-    ../configure --disable-werror TARGET_CC=$TARGET-gcc TARGET_OBJCOPY=$TARGET-objcopy \
-    TARGET_STRIP=$TARGET-strip TARGET_NM=$TARGET-nm TARGET_RANLIB=$TARGET-ranlib --target=$TARGET --prefix=$PREFIX
-    make
-    make install
-  fi
-
-  # Install rust
-  curl https://sh.rustup.rs -sSf | sh
-  export PATH="$HOME/.cargo/bin"
-  rustup override add nightly
 }
 
 archLinux() {
