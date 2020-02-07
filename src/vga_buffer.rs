6de6fd6ee7d92514d4a91beda29a0abce4619ad6
Eugene Obrezkov <ghaiklor@gmail.com>
2020-02-07 22:04:18 +0200

    Implement test runner and few test cases

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -4,6 +4,9 @@ use lazy_static::lazy_static;
 use spin::Mutex;
 use volatile::Volatile;
 
+#[cfg(test)]
+use crate::{serial_print, serial_println};
+
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
@@ -141,4 +144,37 @@ lazy_static! {
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) }
     });
+}
+
+#[test_case]
+fn test_println_simple() {
+    serial_print!("test_println_simple...");
+    println!("test_println_simple output");
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_many() {
+    serial_print!("test_println_many...");
+
+    for _i in 0..200 {
+        println!("test_println_many output");
+    }
+
+    serial_println!("[ok]");
+}
+
+#[test_case]
+fn test_println_output() {
+    serial_print!("test_println_output...");
+
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+
+    serial_println!("[ok]")
 }
