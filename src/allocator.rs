eadab9d7288dfd1735ab0abff00c1b46d86e93c2
Eugene Obrezkov <ghaiklor@gmail.com>
2020-02-23 12:43:06 +0200

    Implement Fixed Size Block Allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -6,11 +6,12 @@ use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, S
 use x86_64::VirtAddr;
 
 pub mod bump;
+pub mod fixed_size_block;
 pub mod linked_list;
 
 #[global_allocator]
-static ALLOCATOR: Locked<linked_list::LinkedListAllocator> =
-    Locked::new(linked_list::LinkedListAllocator::new());
+static ALLOCATOR: Locked<fixed_size_block::FixedSizeBlockAllocator> =
+    Locked::new(fixed_size_block::FixedSizeBlockAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 #![feature(abi_x86_interrupt)]
 #![feature(alloc_layout_extra)]
 #![feature(const_fn)]
+#![feature(const_in_array_repeat_expressions)]
 #![feature(alloc_error_handler)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
